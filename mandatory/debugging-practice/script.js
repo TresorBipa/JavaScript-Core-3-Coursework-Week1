7781ad8c5e4f977f970546f60618273ed0540101
Tresor83 <66829445+Tresor83@users.noreply.github.com>
2022-09-20 00:06:31 +0200

    Done

--- mandatory/debugging-practice/script.js
+++ mandatory/debugging-practice/script.js
@@ -1,5 +1,4 @@
 let myLibrary = [];
-
 window.addEventListener("load", function (e) {
   populateStorage();
   render();
@@ -7,19 +6,14 @@ window.addEventListener("load", function (e) {
 
 function populateStorage() {
   if (myLibrary.length == 0) {
-    let book1 = new Book("Robison Crusoe", "Daniel Defoe", "252", true);
-    let book2 = new Book(
-      "The Old Man and the Sea",
-      "Ernest Hemingway",
-      "127",
-      true
-    );
+    
+    let book1 = new Book("Robinson Crusoe", "Daniel Defoe", "252", true);
+    let book2 = new Book("The Old Man and the Sea","Ernest Hemingway","127",true);
     myLibrary.push(book1);
     myLibrary.push(book2);
     render();
   }
 }
-
 const title = document.getElementById("title");
 const author = document.getElementById("author");
 const pages = document.getElementById("pages");
@@ -27,6 +21,7 @@ const check = document.getElementById("check");
 
 //check the right input from forms and if its ok -> add the new book (object in array)
 //via Book function and start render function
+
 function submit() {
   if (
     title.value == null ||
@@ -40,6 +35,12 @@ function submit() {
     let book = new Book(title.value, title.value, pages.value, check.checked);
     library.push(book);
     render();
+
+    //The code below will clear all fields after a new book has been added
+
+    title.value = ""
+    author.value = ""
+    pages.value = ""
   }
 }
 
@@ -49,15 +50,17 @@ function Book(title, author, pages, check) {
   this.pages = pages;
   this.check = check;
 }
-
 function render() {
   let table = document.getElementById("display");
   let rowsNumber = table.rows.length;
+
   //delete old table
-  for (let n = rowsNumber - 1; n > 0; n-- {
+
+  for (let n = rowsNumber - 1; n > 0; n-- ){
     table.deleteRow(n);
   }
   //insert updated row and cells
+
   let length = myLibrary.length;
   for (let i = 0; i < length; i++) {
     let row = table.insertRow(1);
@@ -71,30 +74,31 @@ function render() {
     cell3.innerHTML = myLibrary[i].pages;
 
     //add and wait for action for read/unread button
+
     let changeBut = document.createElement("button");
     changeBut.id = i;
     changeBut.className = "btn btn-success";
     cell4.appendChild(changeBut);
     let readStatus = "";
-    if (myLibrary[i].check == false) {
+    if (myLibrary[i].check == true) {
       readStatus = "Yes";
     } else {
       readStatus = "No";
     }
     changeBut.innerHTML = readStatus;
-
     changeBut.addEventListener("click", function () {
       myLibrary[i].check = !myLibrary[i].check;
       render();
     });
 
     //add delete button to every row and render again
-    let delButton = document.createElement("button");
-    delBut.id = i + 5;
+
+    let delBut = document.createElement("button");
+    delBut.id = i + 3;
     cell5.appendChild(delBut);
     delBut.className = "btn btn-warning";
     delBut.innerHTML = "Delete";
-    delBut.addEventListener("clicks", function () {
+    delBut.addEventListener("click", function () {
       alert(`You've deleted title: ${myLibrary[i].title}`);
       myLibrary.splice(i, 1);
       render();
